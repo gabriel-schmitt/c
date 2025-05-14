cbf3f30689261e78c154f2c7443ae1940a6779d3
Gabriel Schmitt <gabrielrschmitt@gmail.com>
2025-05-14 00:42:56 -0300

    Adiciona solução completa para o problema do BeeCrowd 'Manutenção' (3044)

--- manutencao_3044.cpp
+++ manutencao_3044.cpp
@@ -1,5 +1,9 @@
+// codigo de usuario: 825667
+// Manutencao: 3044
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TAM 400
 
@@ -47,6 +51,9 @@ int lowpt(int v, int n) {
 int main() {
     int M, N, caseIdx = 1;
 
+    char *buffer = NULL;
+    size_t size = 0;
+
     while(1) {
         scanf("%d %d", &N, &M);
         if(M == 0 && N == 0) break;
@@ -63,7 +70,8 @@ int main() {
         dfs(1, 0, N+1);
         lowpt(1, N+1);
 
-        int art[N-2], head=0, tail=0;
+        int *art = (int*)malloc((N-2) * sizeof(int));
+        int head = 0, tail = 0;
         for (int i=1; i<=N; i++) {
             if (level[i] != 0) {
                 for (int j=1; j<=N; j++) {
@@ -85,18 +93,28 @@ int main() {
             }
         }
 
-        printf("Teste %d\n", caseIdx++);
-        if (tail == 0) 
-            printf("nenhum\n");
-        else 
+        char output[1024];
+        int offset = snprintf(output, sizeof(output), "Teste %d\n", caseIdx++);
+        if (tail == 0) {
+            offset += snprintf(output + offset, sizeof(output) - offset, "nenhum\n\n");
+        } else {
             qsort(art, tail, sizeof(int), compare);
-            while (head != tail)
-                printf("%d ", art[head++]);
+            while (head != tail) {
+                offset += snprintf(output + offset, sizeof(output) - offset, "%d ", art[head++]);
+            }
+            offset += snprintf(output + offset, sizeof(output) - offset, "\n\n");
+        }
 
-        printf("\n");
+        size_t newSize = size + offset + 1;
+        buffer = (char*)realloc(buffer, newSize);
+        strcpy(buffer + size, output);
+        size = newSize - 1;
 
+        free(art);
         free(G);
     }
+
+    printf("%s", buffer);
     
     return 0;
 }
