36509b7cccd689db4b45a7c3be3390cf8438deae
Gabriel Schmitt <gabrielrschmitt@gmail.com>
2025-06-11 03:15:23 -0300

    Códido para solução de Gopher II atualizado e é versão aceita na plataforma!

--- gopher_2.c
+++ gopher_2.c
@@ -1,3 +1,5 @@
+// ACCEPTED!!!
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,7 +7,7 @@
 
 #define MAXN 100
 #define MAXM 100
-#define MAXV 300
+#define MAXV 202
 #define INF 1000000000
 
 #define cont(i,j) F[j][i]
@@ -19,7 +21,7 @@ double gx[MAXN], gy[MAXN];
 double hx[MAXM], hy[MAXM];
 
 int n, m, s, v;
-int S, T;
+int S, T, V=MAXV;
 
 double dist(double x1, double y1, double x2, double y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
@@ -32,7 +34,8 @@ void add_edge(int v, int w) {
 int dfs(int s, int t, int min) {
     visited[s] = 1;
     if (s == t) return min;
-    for (int i = 0; i < MAXV; i++) {
+    int i;
+    for (i = 0; i < MAXV; i++) {
         if (visited[i]) continue;
         int contrario = cont(s, i);
         int direto = dir(s, i);
@@ -53,27 +56,30 @@ int main() {
     while (scanf("%d %d %d %d", &n, &m, &s, &v) != EOF) {
         S = 0;
         T = 1;
+        V = 2 + n + m;
         int g_offset = 2;
         int h_offset = 2 + n;
 
         memset(cap, 0, sizeof(cap));
         memset(F, 0, sizeof(F));
 
-        for (int i = 0; i < n; i++)
+        int i;
+        for (i = 0; i < n; i++)
             scanf("%lf %lf", &gx[i], &gy[i]);
-        for (int i = 0; i < m; i++)
+        for (i = 0; i < m; i++)
             scanf("%lf %lf", &hx[i], &hy[i]);
 
         double max_dist = s * v;
 
-        for (int i = 0; i < n; i++)
+        for (i = 0; i < n; i++)
             add_edge(S, g_offset + i);
 
-        for (int i = 0; i < m; i++)
+        for (i = 0; i < m; i++)
             add_edge(h_offset + i, T);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (i = 0; i < n; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
                 if (dist(gx[i], gy[i], hx[j], hy[j]) <= max_dist) {
                     add_edge(g_offset + i, h_offset + j);
                 }
